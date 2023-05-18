a9eb6f51d09d1e4f7f2a3fc335eb6cef3cee870d
JeongYunMi <tamd5971@gmail.com>
2023-05-18 23:13:50 +0900

    feat: Tag component storybook 작성 완료

--- stories/commonComponents/tag.stories.tsx
+++ stories/commonComponents/tag.stories.tsx
@@ -1,74 +1,56 @@
-import { withKnobs, text, boolean, button, select } from '@storybook/addon-knobs';
+import type { Meta, StoryObj } from '@storybook/react';
 
 import Tag from '../../src/components/Tag';
-import type { tagValue } from '../../src/types/componentsProps';
 
-export default {
+const meta: Meta<typeof Tag> = {
   title: 'commonComponents/Tag',
   component: Tag,
-  decorators: [withKnobs],
+  tags: ['test'],
+  argTypes: {
+    title: {
+      control: {
+        type: 'text',
+      },
+      defaultValue: '공지사항',
+    },
+    fontSize: {
+      control: {
+        type: 'text',
+      },
+      defaultValue: '12px',
+    },
+    length: {
+      control: {
+        type: 'select',
+        options: ['short', 'medium', 'long'],
+      },
+      defaultValue: '12px',
+    },
+    clickAble: {
+      control: {
+        type: 'boolean',
+      },
+      defaultValue: true,
+    },
+    onClickHandler: {
+      control: {
+        type: 'function',
+      },
+      defaultValue: ()=>{},
+    },
+  },
 };
 
-export const option1 = () => {
-  const title = text('title', '줄이기');
-  const length = select('length', ['short', 'medium', 'long'], 'short');
-  const clickAble = boolean('clickAble', true);
-  const onClickHandler = button('onClickHandler', () => { console.log('click') });
+export default meta;
 
-  return <Tag title={title} length={length} clickAble={clickAble} onClickHandler={onClickHandler} />;
+type Story = StoryObj<typeof Tag>;
+
+export const Primary: Story = {
+  args: {
+    title: '공지사항',
+    length: 'long',
+    clickAble: true,
+    onClickHandler: ()=>{alert('click!')}
+  },
 };
 
-option1.storyName = 'Option 1';
-
-export const option2 = () => {
-  const title = text('title', 'HOT!');
-  const length = select('length', ['short', 'medium', 'long'], 'short');
-  const clickAble = boolean('clickAble', false);
-
-  return <Tag title={title} length={length} clickAble={clickAble} />;
-};
-
-option2.storyName = 'Option 2';
-
-export const option3 = () => {
-  const title = text('title', '이벤트');
-  const length = select('length', ['short', 'medium', 'long'], 'medium');
-  const clickAble = boolean('clickAble', false);
-
-  return <Tag title={title} length={length} clickAble={clickAble} />;
-};
-
-option3.storyName = 'Option 3';
-
-export const option4 = () => {
-  const title = text('title', '서울 - 양천구');
-  const length = select('length', ['short', 'medium', 'long'], 'medium');
-  const fontSize = text('fontSize', '16px')
-  const clickAble = boolean('clickAble', true);
-  const onClickHandler = button('onClickHandler', () => { console.log('click') });
-
-  return <Tag title={title} fontSize={fontSize} length={length} clickAble={clickAble} onClickHandler={onClickHandler} />;
-};
-
-option4.storyName = 'Option 4';
-
-
-export const option5 = () => {
-  const title = text('title', '더보기');
-  const length = select('length', ['short', 'medium', 'long'], 'long');
-  const clickAble = boolean('clickAble', true);
-  const onClickHandler = button('onClickHandler', () => { console.log('click') });
-
-  return <Tag title={title} length={length} clickAble={clickAble} onClickHandler={onClickHandler} />;
-};
-
-option5.storyName = 'Option 5';
-
-/* <Tag title='줄이기' length='short' clickAble={false}  />
-      <Tag title='HOT!'  length='short' clickAble={false}  />
-
-      <Tag title='이벤트'  length='medium' clickAble={false}  />
-      <Tag title='서울 - 양천구' length='medium' fontSize='16px' clickAble={true} onClickHandler={() => handleClick(12)} />
-
-
-      <Tag title='더보기' length='long' clickAble={true} onClickHandler={() => handleClick(12)} /> */
