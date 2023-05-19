a13be39fdf61611d7906d1ffe3bb49775d16a783
donggyun <donggyunkim12@gmail.com>
2023-05-19 13:52:57 +0900

    feat: mapPage ui 구성 #4

---
+++ src/container/mapPage/components/AroundMarketDiv.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const AroundMarketDiv = () => {
+  return <Container></Container>;
+};
+
+export default AroundMarketDiv;
+
+const Container = styled.div``;
