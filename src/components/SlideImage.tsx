20a28e1f8bde6c37a92b837c1024c4a2cf23e626
JeongYunMi <tamd5971@gamil.com>
2023-05-23 14:00:32 +0900

    feat: SlideImage Component 개발 및 storybook 작성

--- src/components/SlideImage.tsx
+++ src/components/SlideImage.tsx
@@ -1,25 +1,57 @@
 import Image from 'next/image';
 import { slideImageValue } from '../types/componentsProps';
 import { useState } from 'react';
-import { ImageWrap, MoveButton } from './SlideImage.style';
+import { Wrapper, BookmarkOnWrap, BookmarkOffWrap, ImageWrap, RightMoveButton, LeftMoveButton, BottomComponent } from './SlideImage.style';
  
-function SlideImage({scrArray, width, height, bookmark=false, children=<></>}:slideImageValue) {
+function SlideImage({scrArray, width=384, height=384, bookmark=false, onBookmark=false, children=<></>}:slideImageValue) {
   const [imgCnt, setImgCnt] = useState<number>(0);
-  const maxImgCnt = scrArray.length;
+  const [onBookmarkState, setOnBookmarkState] = useState<boolean>(onBookmark);
+  const maxImgCnt = scrArray.length -1;
+
+  const onClickMoveLeft = () => {
+    if(imgCnt === 0){
+      return;
+    }else{
+      setImgCnt(prev => prev-1)
+    }
+  }
+
+  const onClickMoveRight = () => {
+    if(imgCnt === maxImgCnt){
+      return;
+    }else{
+      setImgCnt(prev => prev+1)
+    }
+  }
+
+  const onClickBookmark = () => {
+    setOnBookmarkState(prev => !prev)
+  }
 
   return (
-    <ImageWrap>
-        <MoveButton/>
-        <MoveButton/>
+    <Wrapper width={width} height={height}>
+      {
+        imgCnt !== 0 && <LeftMoveButton width={width} height={height} onClick={onClickMoveLeft}/>
+      }
+      {
+        imgCnt !== maxImgCnt && <RightMoveButton width={width} height={height} onClick={onClickMoveRight}/>
+      }
+      {
+        bookmark && ( onBookmarkState ? <BookmarkOnWrap width={width} height={height} onClick={onClickBookmark}/> : <BookmarkOffWrap width={width} height={height} onClick={onClickBookmark} /> )
+      }
+      <ImageWrap>
         <Image
         src={scrArray[imgCnt]}
-        width={500}
-        height={500}
+        width={width}
+        height={height}
+        objectFit="cover"
         alt="Picture of the author"
-        >
-            
-        </Image>
-    </ImageWrap>
+        />
+      </ImageWrap>
+      <BottomComponent>
+        {children}
+      </BottomComponent>
+    </Wrapper>
   );
 }
 
--- stories/commonComponents/slideImage.stories.tsx
+++ stories/commonComponents/slideImage.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import SlideImage from '../../src/components/SlideImage';
+import Tag from '../../src/components/Tag';
 
 const meta: Meta<typeof SlideImage> = {
   title: 'commonComponents/SlideImage',
@@ -51,11 +52,44 @@ export const option1: Story = {
   args: {
     scrArray: [
       'https://cdn.pixabay.com/photo/2016/11/22/18/52/cake-1850011_960_720.jpg',
-      'https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971552_960_720.jpg'
+      'https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971552_960_720.jpg',
+      'https://cdn.pixabay.com/photo/2018/09/11/11/47/cake-3669245_640.jpg'
     ],
-    height:80,
-    width: 80,
+    height:400,
+    width: 400,
     bookmark: false,
     children: <></>
+  },
+};
+
+
+export const option2: Story = {
+  args: {
+    scrArray: [
+      'https://cdn.pixabay.com/photo/2016/11/22/18/52/cake-1850011_960_720.jpg',
+      'https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971552_960_720.jpg',
+      'https://cdn.pixabay.com/photo/2018/09/11/11/47/cake-3669245_640.jpg'
+    ],
+    height:400,
+    width: 400,
+    bookmark: true,
+    onBookmark: true,
+    children: <Tag title='HOT!' padding='8px 32px' clickAble={false}  />
+  },
+};
+
+
+export const option3: Story = {
+  args: {
+    scrArray: [
+      'https://cdn.pixabay.com/photo/2016/11/22/18/52/cake-1850011_960_720.jpg',
+      'https://cdn.pixabay.com/photo/2017/01/11/11/33/cake-1971552_960_720.jpg',
+      'https://cdn.pixabay.com/photo/2018/09/11/11/47/cake-3669245_640.jpg'
+    ],
+    height:400,
+    width: 400,
+    bookmark: true,
+    onBookmark: false,
+    children: <Tag title='NEW' padding='8px 32px' clickAble={false}  />
   },
 };
