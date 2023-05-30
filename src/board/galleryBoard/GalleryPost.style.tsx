f55f7a604dc2dc05141021303beaa2a3db10e43d
JeongYunMi <tamd5971@gamil.com>
2023-05-30 16:56:37 +0900

    feat: 게시글 레이아웃 작성 진행중 #6

---
+++ src/board/galleryBoard/GalleryPost.style.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const GalleryPostWrap = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+export const informationWrap = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 456px;
+    background-color: #FCF0E1;
+    padding: 32px 32px 20px 32px;
+`
+
+export const TitleText = styled.div`
+    font-size: 28px;
+    font-family: noto-sans-cjk-kr;
+    font-weight: bold;
+`
+
+export const LocationText = styled.div`
+    font-size: 16px;
+    font-family: noto-sans-cjk-kr;
+    margin-top: 8px;
+`
+
+export const Summary = styled.div`
+    margin: 12px 0 24px 0;
+    font-size: 16px;
+    font-family: noto-sans-cjk-kr;
+    font-weight: bold;
+`
+
