3b4c1ea81e87c1dc6d26bbbd97b5d3783411604a
JeongYunMi <tamd5971@gmail.com>
2023-07-05 19:48:59 +0900

    fix: 배너 사진 출력 에러 수정 진행 중

--- src/board/galleryBoard/GalleryBoardContainer.tsx
+++ src/board/galleryBoard/GalleryBoardContainer.tsx
@@ -8,7 +8,7 @@ import TopTitleImage from './../../../public/assets/galleryboard/TopTitleImage.p
 function GalleryBoardContainer() {
   return (
     <Wrapper>
-        <BoardTop title='가게 게시판' decription='다양한 가게의 게시물을 볼 수 있는 가게 게시판입니다.' imgSrc={TopTitleImage}/>
+        <BoardTop title='가게 게시판' decription='다양한 가게의 게시물을 볼 수 있는 가게 게시판입니다.' imgSrc={''}/>
         <BoardOption/>
         <Contents/>
     </Wrapper>
