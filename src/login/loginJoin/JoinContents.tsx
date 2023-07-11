ecb54eb621030d41b548d4dc448b45c221ac453f
Joonhyung Choi <joonhyungaaa@gmail.com>
2023-07-11 14:55:13 +0900

    feat: login/join 페이지 반응형 레이아웃 적용 #5
    feat: login/join 페이지 반응형 레이아웃 적용

--- src/login/loginJoin/JoinContents.tsx
+++ src/login/loginJoin/JoinContents.tsx
@@ -10,14 +10,28 @@ function JoinContents() {
       <Input placeholder="닉네임 입력" />
       <Input placeholder="이메일 입력" />
       <Input placeholder="비밀번호 입력" />
-      <Tag
-        title="회원가입"
-        width="500px"
-        height="60px"
-        inversion={true}
-        clickAble={true}
-        onClickHandler={() => {}}
-      />
+
+      <TagDiv wrapperWidth={1920}>
+        <Tag
+          title="회원가입"
+          width="500px"
+          height="60px"
+          inversion={true}
+          clickAble={true}
+          onClickHandler={() => {}}
+        />
+      </TagDiv>
+      <TagDiv wrapperWidth={1280}>
+        <Tag
+          title="회원가입"
+          width="333px"
+          height="40px"
+          fontSize="11px"
+          inversion={true}
+          clickAble={true}
+          onClickHandler={() => {}}
+        />
+      </TagDiv>
     </JoinContentsWrapper>
   );
 }
@@ -25,8 +39,22 @@ function JoinContents() {
 export default JoinContents;
 
 const JoinContentsWrapper = styled.div`
+  @media screen and (min-width: 1920px) {
+    height: 400px;
+  }
+  @media screen and (max-width: 1919px) {
+    height: 283px;
+  }
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  height: 400px;
+`;
+
+const TagDiv = styled.div<{ wrapperWidth: 1280 | 1920 }>`
+  @media screen and (min-width: 1920px) {
+    display: ${(props) => (props.wrapperWidth === 1920 ? "" : "none")};
+  }
+  @media screen and (max-width: 1919px) {
+    display: ${(props) => (props.wrapperWidth === 1280 ? "" : "none")};
+  }
 `;
