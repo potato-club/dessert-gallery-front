54fb60f7fa01a4652f8bf5cf247d4e7253643569
donggyun <donggyunkim12@gmail.com>
2023-07-13 17:48:42 +0900

    feat: 공지사항 ui 기능 개발 #33

--- src/container/storePage/StorePage.tsx
+++ src/container/storePage/StorePage.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Calender from "./components/Calender";
 import StoreProfile from "./components/StoreProfile";
 import Announce from "./components/Announce";
 import StoreContent from "./components/StoreContent";
+import Tag from "../../components/Tag";
+
 const StorePage = () => {
+  const [foldClick, setFoldClick] = useState<boolean>(false);
   return (
     <Container>
       <StoreInfo>
@@ -12,6 +15,25 @@ const StorePage = () => {
         <Calender />
       </StoreInfo>
       <Announce />
+      <AnnounceList>
+        {!foldClick && (
+          <AbsoluteDiv foldClick={foldClick}>
+            <Announce />
+            <Announce />
+            <InnerDiv>
+              <FoldBtn
+                title="접기"
+                width="150px"
+                height="48px"
+                clickAble={true}
+                inversion={true}
+                hoverCss={true}
+                onClickHandler={() => setFoldClick(true)}
+              />
+            </InnerDiv>
+          </AbsoluteDiv>
+        )}
+      </AnnounceList>
       <StoreContent />
     </Container>
   );
@@ -35,3 +57,29 @@ const StoreInfo = styled.div`
   width: 100%;
   height: 465px;
 `;
+const AnnounceList = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  gap: 16px;
+`;
+const AbsoluteDiv = styled.div<{ foldClick: boolean }>`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 16px;
+  position: absolute;
+  background-color: #fcf0e1;
+  box-shadow: ${({ foldClick }) =>
+    !foldClick && `0px 3px 6px rgba(0, 0, 0, 0.161);`};
+  width: 100%;
+  padding-top: 16px;
+`;
+const InnerDiv = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  width: 1100px;
+  margin: 8px 0px 24px;
+`;
+const FoldBtn = styled(Tag)``;
