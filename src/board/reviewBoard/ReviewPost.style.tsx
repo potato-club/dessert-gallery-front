f46136d2b493ba05a3bd229140a091e15bfe1855
JeongYunMi <tamd5971@gmail.com>
2023-07-25 18:27:34 +0900

    feat: 후기게시판 게시글 UI 구현

--- src/board/reviewBoard/ReviewPost.style.tsx
+++ src/board/reviewBoard/ReviewPost.style.tsx
@@ -6,6 +6,29 @@ interface componentProps {
     size?: string
 }
 
+interface textComponentProps {
+    color?: string
+    size?: string
+    bold?: boolean
+    marginRight?: boolean
+}
+
+export const Box = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+
+export const BoxContentsStart = styled(Box)`
+    padding: 17px 17px 15px 17px;
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    align-items: flex-start;
+    cursor: pointer;
+`
 export const ReviewPostWrap = styled.div<componentProps>`
     width: ${({width}) => `${width}px`};
     height: ${({height}) => `${height}px`};
@@ -22,7 +45,7 @@ export const InformationWrap = styled.div<componentProps>`
     flex-direction: column;
     width: ${({width}) => `${width}px`};
     align-items: start;
-    padding: 17px 16px 20px 16px;
+    padding-bottom: 20px;
     border-radius: 0 0 16px 16px;
 `
 
@@ -34,7 +57,7 @@ export const TitleText = styled.div<componentProps>`
 `
 
 export const Summary = styled.div<componentProps>`
-    margin: 9px 0 20px 0;
+    margin-top: 9px;
     font-size: ${({size}) => `${size}`};
     font-family: noto-sans-cjk-kr;
     font-weight: bold;
@@ -45,4 +68,45 @@ export const ReviewListWrap = styled.div`
     width: 309px;
     height: 202px;
     background-color: #FFF7EB;
+`
+
+export const ReviewWrap = styled.div`
+    width: 273px;
+    height: 100px;
+    border-top: 3px solid #FF8D00;
+`
+
+export const TopTextWrap = styled.div`  
+    margin: 10px 0 8px 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;    
+`
+
+export const Text = styled.div<textComponentProps>`
+    color: ${({color}) => `${color}`};
+    font-size: ${({size}) => `${size}`};
+    line-height: normal;
+    font-family: noto-sans-cjk-kr;
+    
+    ${({bold}) => {
+        if(bold === true){
+            return `font-weight: bold;`
+        }
+    }};
+    ${({marginRight}) => {
+        if(marginRight === true){
+            return `margin-right: 72px;`
+        }
+    }};
+`
+
+export const TagWrap = styled.div`
+    width: 273px;
+    height: 64px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-top: 3px solid #FF8D00;
+
 `
--- src/board/reviewBoard/ReviewPost.tsx
+++ src/board/reviewBoard/ReviewPost.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ReviewPostWrap, TitleText, InformationWrap, Summary, ReviewListWrap} from './ReviewPost.style'
+import { ReviewPostWrap, TitleText, InformationWrap, Summary, Box, BoxContentsStart, ReviewWrap, Text, TopTextWrap, TagWrap} from './ReviewPost.style'
 import SlideImage from '../../components/SlideImage/SlideImage'
 import Rating from '../../components/Rating'
 import Tag from '../../components/Tag'
@@ -7,38 +7,39 @@ import { ReviewPostValue } from '../../types/componentsProps'
 
 
 export default function ReviewPost({width,height,title, imgSrc,summary,  reviewList}:ReviewPostValue) {
+  /**차후 분리 */
+  const DateToString = reviewList.map(e=> `${e.date.getFullYear()}-${(e.date.getMonth())+1}-${e.date.getDay()}`)
   return (
     <ReviewPostWrap width={width} height={height}>
       
       <SlideImage srcArray={[imgSrc]} width={width} height={height/10*4} bookmark={false} moveBtnType='none' />
+      <BoxContentsStart>
+        <InformationWrap>
+          <TitleText size='13px'>{title}</TitleText>
+          <Summary size='15px'>{summary}</Summary>
 
-      <InformationWrap>
-        <TitleText size='13px'>{title}</TitleText>
-        <Summary size='15px'>{summary}</Summary>
-      </InformationWrap>
+        </InformationWrap>
+        <ReviewWrap>
+          <TopTextWrap >
+            <Text size='13px' color='#FF8D00' bold={true}>{reviewList[0].userId}</Text>
+            <Text size='13px' color='#828282' marginRight={true}>{DateToString[0]}</Text>
+            <Rating size='medium' ratingValue={reviewList[0].rating}/>
+          </TopTextWrap>
+          <Text size='11px' color='#000000' bold={true}>{reviewList[0].contents}</Text>
+        </ReviewWrap>
 
-      <ReviewListWrap>
-      {/* <ReviewWrap>
-        <TopText >
-          <UserIdText>{reviewList[0].userId}</UserIdText>
-          <DateText>{reviewList[0].date}</DateText>
-          <Rating size='medium' ratingValue={reviewList[0].rating}/>
-        </TopText>
-        <ContentsText>{reviewList[0].contents}</ContentsText>
-      </ReviewWrap>
-
-      <ReviewWrap>
-        <TopText >
-          <UserIdText>{reviewList[1].userId}</UserIdText>
-          <DateText>{reviewList[1].date}</DateText>
-          <Rating size='medium' ratingValue={reviewList[1].rating}/>
-        </TopText>
-        <ContentsText>{reviewList[1].contents}</ContentsText>
-      </ReviewWrap> */}
-      </ReviewListWrap>
-
-      <Tag width='164px' height='32px' title='더보기' fontSize='12px' clickAble={true} hoverCss={true} onClickHandler={()=>{alert('click!')}}/>
-
+        <ReviewWrap>
+          <TopTextWrap >
+            <Text size='13px' color='#FF8D00' bold={true}>{reviewList[1].userId}</Text>
+            <Text size='13px' color='#828282' marginRight={true}>{DateToString[1]}</Text>
+            <Rating size='medium' ratingValue={reviewList[1].rating}/>
+          </TopTextWrap>
+          <Text size='11px' color='#000000' bold={true}>{reviewList[1].contents}</Text>
+        </ReviewWrap> 
+        <TagWrap>
+          <Tag width='164px' height='32px' title='더보기' fontSize='12px' clickAble={true} hoverCss={true} onClickHandler={()=>{alert('click!')}}/>
+        </TagWrap>
+      </BoxContentsStart>
     </ReviewPostWrap>
   )
 }
