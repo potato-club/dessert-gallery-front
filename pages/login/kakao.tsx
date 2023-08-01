3d966ac05b1e420600b9e63429ff2f57c7b66af6
Joonhyung Choi <joonhyungaaa@gmail.com>
2023-08-01 16:33:32 +0900

    feat: 카카오 로그인 시 인가 코드를 받아와 백엔드로 전송해주는 login/kakao 페이지(프로토타입) 추가 #5
    feat: 카카오 로그인 시 인가 코드를 받아와 백엔드로 전송해주는 login/kakao 페이지(프로토타입) 추가

---
+++ pages/login/kakao.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
+import { useRecoilState } from "recoil";
+import { signUpDataState } from "../../src/recoil/login/signUpState";
+import Wrapper from "../../src/login/components/Wrapper";
+
+const Kakao = () => {
+  const router = useRouter();
+  const [signUpData, setSignUpData] = useRecoilState(signUpDataState);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let code = new URL(window.location.href).searchParams.get("code");
+
+      if (code !== undefined) {
+        try {
+          const response = await axios.get(
+            `https://api.dessert-gallery.site/users/login/kakao?code=${code}`
+          );
+          console.log(response.data);
+          setSignUpData({
+            ...signUpData,
+            email: response.data.email,
+            loginType: "KAKAO",
+          });
+
+          if (response.data.responseCode === "200") {
+            // 여기는 로그인 처리 후 메인페이지로
+            console.log(200);
+          } else if (response.data.responseCode === "201") {
+            // 여기는 신규 회원이니 회원가입으로
+            console.log(201);
+            router.replace({
+              pathname: "/login/pick",
+            });
+          } else {
+            console.log("에러", response.data.responseCode);
+          }
+        } catch (error) {
+          console.log("Error occurred:", error);
+        }
+      }
+    };
+
+    const response = fetchData();
+  }, [router, setSignUpData, signUpData]);
+  return <Wrapper>카카오 로그인 테스트 페이지</Wrapper>;
+};
+
+export default Kakao;
