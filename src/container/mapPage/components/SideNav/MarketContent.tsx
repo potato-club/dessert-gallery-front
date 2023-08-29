39d675a8e248be875f23ade27b3007230e376bad
donggyun <donggyunkim12@gmail.com>
2023-08-29 19:56:18 +0900

    feat: mapPage ui 개발중 #4

--- src/container/mapPage/components/SideNav/MarketContent.tsx
+++ src/container/mapPage/components/SideNav/MarketContent.tsx
@@ -5,7 +5,7 @@ import AroundMarketItem from "./AroundMarketItem";
 const MarketContent = () => {
   return (
     <Container>
-      <TopSpan>내 근처 가게 알아보기</TopSpan>
+      <TopSpan>내 근처에 무슨 가게가 있을까요?</TopSpan>
       <MarketList>
         <AroundMarketItem />
         <AroundMarketItem />
@@ -23,9 +23,23 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   gap: 7px;
+  padding: 20px 29px 32px;
+  overflow-y: scroll;
+  ::-webkit-scrollbar {
+    width: 7px;
+  }
+  ::-webkit-scrollbar-track {
+    background-color: #fffdf9;
+  }
+  ::-webkit-scrollbar-thumb {
+    background-color: #ff8d00;
+    height: 165px;
+  }
+  ::-webkit-scrollbar-thumb:hover {
+  }
 `;
 const TopSpan = styled.span`
-  font-size: 18px;
+  font-size: 12px;
   font-weight: 700;
 `;
 const MarketList = styled.ul`
