67ee097b7cd5010b2b5b23e8bb5dacafe0c899a6
JeongYunMi <tamd5971@gamil.com>
2023-09-12 15:19:18 +0900

    fix: 보드게시판 기존검색어 변경하여 다시 검색하는 경우 기존 데이터 남아있는 에러 수정

--- src/hooks/useGetStoreBoardList.ts
+++ src/hooks/useGetStoreBoardList.ts
@@ -18,17 +18,30 @@ async function fetchStoreBoardData(req: boardSearchOptionData) {
 
   try {
     const { data } = await boardApiList.getBoardList(queryString);
-    if(data.length ===0){
-      req.setToast(true)
-    }else{
-      if(Number(req.page)-1 >=req.resData.length){
-        let temp = [];
-        if(req.resData.length !== 0)
-          temp.push(req.resData)
-        temp.push(data)
-        req.setResData(temp)
+    
+      // pageCount 1, 첫 검색시, 초기화
+      if(Number(req.page) === 1 ){
+        if(data.length ===0){  // 첫 검색인데 데이터 없을시
+          req.setToast(true);  // 데이터 없다고 띄우기
+          req.setResData([])   // 데이터 초기화
+        }else{                        // 검색해서 받은 데이터 있는 경우
+          let temp = [];
+          temp.push(data)             // 데이터 가공
+          req.setResData(temp)        // 받은 데이터 저장
+        } 
       }
-    }
+
+      // 같은 쿼리로 pageCount만 변경
+      else if(Number(req.page)-1 >=req.resData.length){
+        if(data.length ===0 && req.resData.length !== 0){ // 새로 받은 데이터는 빈배열이나, 기존에 가지고 있는 데이터가 있는 경우
+          req.setToast(true);                             // 데이터 없다고 띄우기
+        }else{                  // 새로 받은 데이터가 있는 경우
+          let temp = req.resData;
+          temp.push(data)     
+          req.setResData(temp)
+        } 
+      }
+    
     return data;
   } catch (error) {
     console.log(error)
@@ -38,10 +51,10 @@ async function fetchStoreBoardData(req: boardSearchOptionData) {
 export function useGetStoreBoardListdData(req: boardSearchOptionData) {
   const queryKey = ['boardData', req.page, req.address, req.searchType, req.sortType];
 
-  // useQuery 훅을 사용하여 데이터 가져오기
+  // useQuery 훅을 사용
   return useInfiniteQuery(queryKey, () => fetchStoreBoardData(req), {
     getNextPageParam: (lastPage, pages) => {
-      // 페이지 번호를 pageCount로 설정합니다.
+      // 페이지 번호를 pageCount로 설정
       return req.page+1;
     },
   });
