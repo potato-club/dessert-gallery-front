64b439114b302b2542668ee884e0e4cd7afdda31
Joonhyung Choi <joonhyungaaa@gmail.com>
2023-09-19 11:44:27 +0900

    feat: login/main 페이지로 이동 시 signupData, kakaoSignupData가 초기화되는 기능 추가 #5
    feat: login/main 페이지로 이동 시 signupData, kakaoSignupData가 초기화되는 기능 추가

--- pages/login/main.tsx
+++ pages/login/main.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import LoginMainContainer from "../../src/container/loginPage/loginMain/LoginMainContainer";
+import { useKakaoSignupDataState } from "../../src/recoil/login/kakaoSignUpStateAtom";
+import {
+  signUpDataType,
+  signupDataStateAtom,
+} from "../../src/recoil/login/signUpStateAtom";
+import { useRecoilState } from "recoil";
 
 const LoginMain = () => {
+  const [signupData, setSignupData] = useRecoilState(signupDataStateAtom);
+  const [kakaoSignupData, setKakaoSignupData] = useKakaoSignupDataState();
+
+  const defaultValue: signUpDataType = {
+    email: "",
+    userRole: "USER",
+    loginType: "NORMAL",
+    nickname: "",
+  };
+
+  useEffect(() => {
+    setSignupData(defaultValue);
+    setKakaoSignupData(defaultValue);
+  }, []);
   return <LoginMainContainer />;
 };
 
