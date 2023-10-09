627f5cad1f68cf75ad7ff007b92894ad7a24d82e
JeongYunMi <tamd5971@gmail.com>
2023-10-10 06:18:01 +0900

    feat: main page api 통신 코드 구현 #70

---
+++ src/apis/controller/mainPage.ts
@@ -0,0 +1,24 @@
+import sendApi from "../sendApi";
+
+export const mainApiList = {
+  getStore: async(option: string) => {
+    if(option === "popularStore"){
+      return (await sendApi.get("/list/stores?page=1&sortType=FOLLOWER")).data
+    }else if(option === "recentStore"){
+      return (await sendApi.get("/list/stores?page=1&sortType=RECENT")).data
+    }else if(option === "recentReview"){
+      return (await sendApi.get("/list/reviews?page=1&sortType=RECENT")).data
+    }
+  },
+
+  getMainFollowBoardList: async() => {
+    if(false){
+      return false;
+    }
+    return (await sendApi.get("/mypage/follow?page=1")).data;
+  },
+
+  getNearbyStore: async() => {
+    return (await sendApi.get("/kakaoMap?lat=37.34701&lon=126.9509&radius=1000")).data;
+  },
+} 
