3df7314d3778b971cef29c2ac4eb1dd9dfb2d763
JeongYunMi <tamd5971@gmail.com>
2023-10-17 19:32:03 +0900

    refactor: api 처리 방식 변경

--- src/apis/sendApi.ts
+++ src/apis/sendApi.ts
@@ -1,41 +1,32 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import axios from 'axios';
-import { AUTH_KEY } from '../constants/authkey';
-import httpService from '../constants/libs/httpService';
 
-let ck = `eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0YW1kNTk3MUBuYXZlci5jb20iLCJyb2xlcyI6Ik1BTkFHRVIiLCJpYXQiOjE2OTY5Mzk0MzgsImV4cCI6MTY5Njk0MTIzOH0.KIktA7E3SWuAUu_o2nlzXz3mEfBHWU3sVaLaR9SpTpM`
 
 export const sendApi = {
   get: (url:string) => {
     return axios.get(
-      `https://api.dessert-gallery.site${url}`,
-      // httpService.authorization(localStorageService.get(SESSION_ID))
-      httpService.authorization(ck)
+      `${process.env.NEXT_PUBLIC_API_URL}${url}`,
     );
   },
 
 
   guestGet: (url: string) => {
-    console.log("getGuest: ", `https://api.dessert-gallery.site${url}`)
     return axios.get(
-      `https://api.dessert-gallery.site${url}`,
+      `${process.env.NEXT_PUBLIC_API_URL}${url}`,
     );
   },
 
   post: (url: string, req: object = {}) => {
     return axios.post(
-      `https://api.dessert-gallery.site${url}`,
+      `${process.env.NEXT_PUBLIC_API_URL}${url}`,
       req,
-      // httpService.authorization(localStorageService.get(SESSION_ID))
-      httpService.authorization(ck)
     );
   },
 
   put: (url: string, req: object = {}) => {
     return axios.put(
-      `https://api.dessert-gallery.site${url}`,
+      `${process.env.NEXT_PUBLIC_API_URL}${url}`,
       req,
-      // httpService.authorization(localStorageService.get(SESSION_ID))
-      httpService.authorization(ck)
     );
   },
 
