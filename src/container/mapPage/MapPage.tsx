3070a7729c0b910226b725c6efe92cd24537f288
JeongYunMi <tamd5971@gmail.com>
2023-11-07 06:56:36 +0900

    feat: mapPage kakaoMap 연동 완료 지역 선택 컴포넌트 구현 #58

--- src/container/mapPage/MapPage.tsx
+++ src/container/mapPage/MapPage.tsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import MarketDetailInfo from "./components/SideNav/MarketInfos";
 import SideNav from "./components/SideNav/SideNav";
 import Maps from "./components/Maps";
 
+interface coord {
+  lat: string
+  lng: string
+}
+
 const MapPage = () => {
+  const [center, setCenter] = useState<coord>({lat: "37.34701", lng: "126.9509"})
+  const [sidebar, setSidebar] = useState<boolean>(true)
   return (
     <Container>
       <SideNav />
       <MarketDetailInfo />
-      <Maps />
+      <Maps centerCoord={center} sidebar={sidebar}/>
     </Container>
   );
 };
--- src/container/mapPage/components/Maps/index.tsx
+++ src/container/mapPage/components/Maps/index.tsx
@@ -1,13 +1,90 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
+import LocationSelector from "../../../boardPage/boardComponents/boardFilterOption/LocationSelector";
 
-const Maps = () => {
-  return <Container>map</Container>;
+interface coord {
+  lat: string
+  lng: string
+}
+
+interface props {
+  centerCoord: coord
+  sidebar: boolean
+}
+
+interface style {
+  sidebar: boolean
+}
+
+const Maps = ({centerCoord, sidebar}: props) => {
+  const mapContainer = useRef<HTMLDivElement>(null);
+  const [moveMap, setMoveMap] = useState<boolean>(false);
+
+  useEffect(()=>{
+
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAPS_API_KEY}&autoload=false`;
+    document.head.appendChild(script);
+
+    // 스크립트 로드가 완료되면 메인 컴포넌트 렌더링
+    script.onload = () => {
+      (window as any).kakao.maps.load(function() {
+        if (mapContainer && mapContainer.current) {
+          let markerPosition  = new (window as any).kakao.maps.LatLng(centerCoord.lat,centerCoord.lng); 
+          var marker = new (window as any).kakao.maps.Marker({
+            position: markerPosition
+        });
+
+          let mapOption = { 
+              center: new (window as any).kakao.maps.LatLng(centerCoord.lat,centerCoord.lng), // 지도의 중심좌표
+              level: 3, // 지도의 확대 레벨
+          };    
+    
+          // 지도를 생성합니다
+          let map = new (window as any).kakao.maps.Map(mapContainer.current, mapOption);
+          marker.setMap(map) ;
+        }
+      });
+    };
+
+    
+  },[])
+
+  return (
+    <Container sidebar={sidebar} ref={mapContainer} >
+      <LocationSelectorBtn/>
+      {moveMap&& <LocationSelector selectedLocation={""} onChangeLocation={function (e: string): void {
+        throw new Error("Function not implemented.");
+      } }/>}
+    </Container>
+  );
 };
 
 export default Maps;
 
-const Container = styled.div`
+const Container = styled.div<style>`
   position: absolute;
-  z-index: 1;
+  right: 0;
+  width: 100%;
+  width: calc(100% - 439px);
+  height: 100%;
+  ${({sidebar}) => {
+        if(sidebar === true){
+            return `width: calc(100% - 439px - 431px);`
+        }
+    }};
 `;
+
+const LocationSelectorBtn = styled.div`
+  width: 35px;
+  height: 35px;
+  background-color: #fcf0e1;
+  border: 2px solid #FF6F00;
+  border-radius: 4px;
+  position: absolute;
+  right: 8px;
+  top: 8px;
+  z-index: 32;
+  cursor: pointer;
+`
