124d6b05bce3be6585658b5cfe206178c6b7ff0d
Joonhyung Choi <joonhyungaaa@gmail.com>
2023-11-12 10:08:48 +0900

    feat: login/pick 페이지 모달 디자인 수정 및 컴포넌트 구조 변경, 닉네임 중복 확인 api 추가, 세부 디테일 추가(닉네임 입력 관련 디테일) #5
    feat: login/pick 페이지 모달 디자인 수정 및 컴포넌트 구조 변경, 닉네임 중복 확인 api 추가, 세부 디테일 추가(닉네임 입력 관련 디테일)

--- src/container/loginPage/components/Modal.tsx
+++ src/container/loginPage/components/Modal.tsx
@@ -10,7 +10,8 @@ import { useRouter } from "next/router";
 function Modal() {
   const router = useRouter();
   const [modalState, setModalState] = useRecoilState(modalStateAtom);
-
+  const [isNicknameChecked, setIsNicknameChecked] = useState(false);
+  const [isDuplicatedNickname, setIsDuplicatedNickname] = useState(false);
   const [signupData, setSignupData] = useSignupDataState();
   const [nickname, setNickname] = useState("");
   const [stepSignup, setStepSignup] = useState(1);
@@ -24,6 +25,27 @@ function Modal() {
 
   const handleNicknameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setNickname(event.target.value);
+  };
+
+  const handleCheckNickname = async () => {
+    const response = await axios.get(
+      "https://api.dessert-gallery.site/users/duplication/nickname",
+      {
+        params: {
+          nickname: nickname,
+        },
+      }
+    );
+    console.log(response);
+    if (response.data === false) {
+      setIsNicknameChecked(true);
+      setStepSignup(stepSignup + 1);
+    } else {
+      setIsDuplicatedNickname(true);
+      setTimeout(() => {
+        setIsDuplicatedNickname(false);
+      }, 2000);
+    }
   };
 
   const handleSignup = async () => {
@@ -45,87 +67,89 @@ function Modal() {
     } catch {}
   };
 
-  const renderModalContent = () => {
+  const stepMessage = () => {
     switch (stepSignup) {
       case 1:
-        return (
-          <>
-            <ModalContentsDiv>
-              <ExplainDiv>
-                {signupData.userRole === "MANAGER"
-                  ? "가게 운영자로 회원가입 하시겠습니까?"
-                  : "일반 회원으로 회원가입 하시겠습니까?"}
-              </ExplainDiv>
-            </ModalContentsDiv>
-            <ButtonDiv>
-              <ModalButton onClick={() => setStepSignup(2)}>확인</ModalButton>
-              <ModalButton onClick={closeModal}>취소</ModalButton>
-            </ButtonDiv>
-          </>
-        );
+        return signupData.userRole === "MANAGER"
+          ? "가게 운영자로 회원가입 하시겠습니까?"
+          : "일반 회원으로 회원가입 하시겠습니까?";
+
       case 2:
-        return (
-          <>
-            <ModalContentsDiv>
-              <ExplainDiv>닉네임을 입력하세요</ExplainDiv>
-              <InputDiv>
-                <NicknameInput
-                  type="text"
-                  onChange={handleNicknameChange}
-                  value={nickname}
-                  placeholder="닉네임을 입력하세요"
-                ></NicknameInput>
-              </InputDiv>
-            </ModalContentsDiv>
-            <ButtonDiv>
-              <ModalButton onClick={() => setStepSignup(3)}>확인</ModalButton>
-              <ModalButton onClick={closeModal}>취소</ModalButton>
-            </ButtonDiv>
-          </>
-        );
+        return "닉네임을 입력하세요";
+
       case 3:
-        return (
-          <>
-            <ModalContentsDiv>
-              <ExplainDiv>
-                닉네임을 {nickname}(으)로 설정하시겠습니까?
-              </ExplainDiv>
-            </ModalContentsDiv>
-            <ButtonDiv>
-              <ModalButton
-                onClick={() => {
-                  handleSignup();
-                }}
-              >
-                확인
-              </ModalButton>
-              <ModalButton onClick={() => setStepSignup(2)}>취소</ModalButton>
-            </ButtonDiv>
-          </>
-        );
+        return `닉네임을 ${nickname}(으)로 설정하시겠습니까?`;
     }
   };
 
-  return <ModalWrapper state={modalState}>{renderModalContent()}</ModalWrapper>;
+  return (
+    <ModalWrapper state={modalState}>
+      <ExplainDiv>{stepMessage()}</ExplainDiv>
+      <InputDiv>
+        <NicknameInput
+          type="text"
+          onChange={handleNicknameChange}
+          value={nickname}
+          placeholder="닉네임을 입력하세요"
+          stepSignup={stepSignup}
+        ></NicknameInput>
+        <NicknameCheckP stepSignup={stepSignup} nickname={nickname}>
+          닉네임이 입력되지 않았습니다.
+        </NicknameCheckP>
+        <DuplicatedNicknameCheckP
+          stepSignup={stepSignup}
+          isDuplicatedNickname={isDuplicatedNickname}
+        >
+          중복된 닉네임이 있습니다.
+        </DuplicatedNicknameCheckP>
+      </InputDiv>
+
+      <ButtonDiv>
+        <ModalButton
+          onClick={() => {
+            if (stepSignup === 3) {
+              handleSignup();
+            } else if (stepSignup === 2) {
+              if (nickname === "") {
+                return;
+              } else {
+                handleCheckNickname();
+              }
+            } else {
+              setStepSignup(stepSignup + 1);
+            }
+          }}
+        >
+          확인
+        </ModalButton>
+        <ModalButton
+          onClick={() => {
+            if (stepSignup === 1) {
+              closeModal();
+            } else {
+              setStepSignup(stepSignup - 1);
+            }
+          }}
+        >
+          취소
+        </ModalButton>
+      </ButtonDiv>
+    </ModalWrapper>
+  );
 }
 
 export default Modal;
 
 const ModalWrapper = styled.div<{ state: boolean }>`
   display: ${(props) => (props.state === false ? "none" : "")};
-  position: relative;
-  border: 1px solid black;
+  border: 3px solid #ff6f00;
+  border-radius: 6px;
   background-color: white;
   position: absolute;
-  width: 500px;
-  height: 300px;
-`;
-
-const ModalContentsDiv = styled.div`
-  box-sizing: border-box;
-  width: 100%;
-  height: 200px;
-  padding: 20px;
+  font-size: 21px;
+  padding: 0 108px;
+  width: 620px;
+  height: 360px;
 `;
 
 const ExplainDiv = styled.div`
@@ -133,32 +157,69 @@ const ExplainDiv = styled.div`
   align-items: center;
   justify-content: center;
   width: 100%;
-  height: 150px;
+  height: 167px;
 `;
 
 const InputDiv = styled.div`
   display: flex;
-  align-items: center;
-  justify-content: center;
+  flex-direction: column;
   width: 100%;
-  height: 50px;
+  height: 92px;
 `;
 
-const NicknameInput = styled.input`
-  width: 300px;
-  height: 30px;
+const NicknameInput = styled.input<{ stepSignup: number }>`
+  display: ${(props) => (props.stepSignup !== 2 ? "none" : "")};
+  width: 100%;
+  height: 40px;
+  padding: 0 20px;
+  font-size: 15px;
+  border-color: #828282;
+  border-radius: 6px;
+`;
+
+const NicknameCheckP = styled.p<{ nickname: string; stepSignup: number }>`
+  display: ${(props) =>
+    props.stepSignup === 2 && props.nickname === "" ? "flex" : "none"};
+  height: 25px;
+  margin-left: 20px;
+  align-items: center;
+  font-size: 11px;
+  color: #ff6f00;
+`;
+
+const DuplicatedNicknameCheckP = styled.p<{
+  isDuplicatedNickname: boolean;
+  stepSignup: number;
+}>`
+  display: ${(props) =>
+    props.stepSignup === 2 && props.isDuplicatedNickname === true
+      ? "flex"
+      : "none"};
+  height: 25px;
+  margin-left: 20px;
+  align-items: center;
+  font-size: 11px;
+  color: #ff6f00;
 `;
 
 const ButtonDiv = styled.div`
   display: flex;
-  align-items: center;
-  justify-content: center;
+  justify-content: space-between;
   width: 100%;
-  height: 100px;
+  height: 101px;
 `;
 
 const ModalButton = styled.button`
-  width: 50px;
-  height: 25px;
-  margin: 20px;
+  width: 170px;
+  height: 50px;
+  border-radius: 6px;
+  background-color: #fcf0e1;
+  border: none;
+  color: #ff6f00;
+  font-size: 18px;
+  font-weight: bold;
+  :hover {
+    background-color: #ff6f00;
+    color: #fcf0e1;
+  }
 `;
