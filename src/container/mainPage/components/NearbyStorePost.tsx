acdc8d44c2077732d27d0bce3fc08479e9de9e5b
JeongYunMi <tamd5971@gamil.com>
2023-11-14 15:38:43 +0900

    fix: mainPage kakao map api 불러오는 방식 변경

--- src/container/mainPage/components/NearbyStorePost.tsx
+++ src/container/mainPage/components/NearbyStorePost.tsx
@@ -13,26 +13,41 @@ function NearbyStorePost({item}: nearbyStoreProps) {
   const storeMap = useRef<HTMLDivElement>(null);
 
   useEffect(()=>{
-      if (storeMap && storeMap.current) {
-        // 이미지 지도에서 마커가 표시될 위치입니다 
-        let markerPosition  = new (window as any).kakao.maps.LatLng(item.latitude, item.longitude); 
 
-        // 이미지 지도에 표시할 마커입니다
-        // 이미지 지도에 표시할 마커는 Object 형태입니다
-        var marker = {
-            position: markerPosition
-        };
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAPS_API_KEY}&libraries=services&autoload=false`;
+    document.head.appendChild(script);
 
-        let staticMapOption = { 
-            center: new (window as any).kakao.maps.LatLng(item.latitude, item.longitude), // 이미지 지도의 중심좌표
-            level: 3, // 이미지 지도의 확대 레벨
-            marker: marker // 이미지 지도에 표시할 마커 
-        };    
+    // 스크립트 로드가 완료되면 메인 컴포넌트 렌더링
+    script.onload = () => {
+      (window as any).kakao.maps.load(function() {
 
-        // 이미지 지도를 생성합니다
-        let staticMap = new (window as any).kakao.maps.StaticMap(storeMap.current, staticMapOption);
-      }
+        if (storeMap && storeMap.current) {
+          // 이미지 지도에서 마커가 표시될 위치입니다 
+          let markerPosition  = new (window as any).kakao.maps.LatLng(item.latitude, item.longitude); 
+  
+          // 이미지 지도에 표시할 마커입니다
+          // 이미지 지도에 표시할 마커는 Object 형태입니다
+          var marker = {
+              position: markerPosition
+          };
+  
+          let staticMapOption = { 
+              center: new (window as any).kakao.maps.LatLng(item.latitude, item.longitude), // 이미지 지도의 중심좌표
+              level: 3, // 이미지 지도의 확대 레벨
+              marker: marker // 이미지 지도에 표시할 마커 
+          };    
+  
+          // 이미지 지도를 생성합니다
+          let staticMap = new (window as any).kakao.maps.StaticMap(storeMap.current, staticMapOption);
+        }
+      });
+    };
+
+
   },[])
+
 
   return (
     <NearbyStorePostWrap key={item.longitude+item.latitude}>
