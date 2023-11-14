c3fc697be8eb32b3e5355e5634ee1fa722312051
JeongYunMi <tamd5971@gamil.com>
2023-11-14 15:07:26 +0900

    feat: ToastMessage 컴포넌트 수정

--- src/components/ToastMessage.tsx
+++ src/components/ToastMessage.tsx
@@ -2,16 +2,19 @@ import React, {useEffect, useState} from 'react'
 import styled from 'styled-components'
 import type { ToastMessageProps } from '../types/componentsProps'
 
-const ToastMessage = ({messageString, timer}:ToastMessageProps) => {
+const ToastMessage = ({messageString, timer, clickEvent=false, eventFunc }:ToastMessageProps) => {
   const [visible, setVisible] = useState(true);
 
   useEffect(()=>{
+    console.log("Toast")
     setTimeout(()=>{setVisible(false)}, timer)
   },[timer])
 
   return visible ? (
     <Wrap>
-      <ToastWrap>{messageString}</ToastWrap>
+      {!clickEvent&&<ToastWrap>{messageString}</ToastWrap>}
+      {clickEvent&&<AbleEventToastWrap onClick={eventFunc}>{messageString}</AbleEventToastWrap>}
+
     </Wrap>
   ):null
 }
@@ -41,3 +44,19 @@ const ToastWrap = styled.div`
   cursor: default;
 `
 
+const AbleEventToastWrap = styled.div`
+  position: fixed;
+  bottom: 20px;
+  left: 45%;
+  width: fit-content;
+  height: fit-content;
+  padding: 8px 12px;
+  font-family: noto-sans-cjk-kr;
+  font-weight: bold;
+  font-size: 16px;
+  border: 3px solid #FDC886;
+  background-color: #ffffff;
+  border-radius: 16px;
+  z-index: 100;
+  cursor: pointer;
+`
