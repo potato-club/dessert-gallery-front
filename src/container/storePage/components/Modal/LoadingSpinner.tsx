4f2dd8000125fcdbbfc09d160e41d32388d74572
donggyun <donggyunkim12@gmail.com>
2023-11-23 19:01:02 +0900

    refactor(LoadingSpinner): props 추가 #33

--- src/container/storePage/components/Modal/LoadingSpinner.tsx
+++ src/container/storePage/components/Modal/LoadingSpinner.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 import styled from "styled-components";
 
-const LoadingSpinner = () => {
-  return <Spinner />;
+interface LoadingSpinnerProps {
+  width: number;
+  height: number;
+  borderWidth: number;
+}
+const LoadingSpinner = ({ ...props }: LoadingSpinnerProps) => {
+  return <Spinner {...props} />;
 };
 
 export default LoadingSpinner;
 
-const Spinner = styled.div`
-  width: 20px;
-  height: 20px;
+const Spinner = styled.div<LoadingSpinnerProps>`
+  width: ${({ width }) => `${width}px`};
+  height: ${({ height }) => `${height}px`};
   box-sizing: border-box;
-  border: 2px solid rgba(0, 0, 0, 0.4);
+  border: ${({ borderWidth }) => `${borderWidth}px`} solid rgba(0, 0, 0, 0.4);
   border-right-color: rgba(0, 0, 0, 0);
   border-radius: 100%;
   animation: spin 1s ease-in-out infinite;
