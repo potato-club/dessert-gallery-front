94155b369daa40fd874100effc2a1d49ba3934f9
donggyun <donggyunkim12@gmail.com>
2023-12-02 16:32:01 +0900

    feat(myPage-MemoList): MemoList 컴포넌트 레이아웃 구현 #67

---
+++ src/container/myPage/calendarPage/components/MemoColumn.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styled from "styled-components";
+
+const MemoColumn = ({ ...props }) => {
+  return (
+    <Container>
+      <input type="checkbox" />
+      <InputContent isSuccess={props.isSuccess}>
+        00님케이크만들기00님케이크만들기만들기
+      </InputContent>
+    </Container>
+  );
+};
+
+export default MemoColumn;
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  input {
+    width: 25px;
+    height: 25px;
+    margin-right: 18px;
+  }
+`;
+const InputContent = styled.span<{ isSuccess: boolean }>`
+  color: ${({ isSuccess }) => (isSuccess ? "#828282" : "#000")};
+  font-family: Noto Sans CJK KR;
+  font-size: 16px;
+  font-weight: 700;
+`;
