4a53c09710148ca83156c129077ce866db57adf1
donggyun <donggyunkim12@gmail.com>
2023-12-04 17:01:11 +0900

    feat(DateModal): 모달 초기 레이아웃

---
+++ src/container/myPage/calendarPage/components/DateModal.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const DateModal = () => {
+  return <Container></Container>;
+};
+
+export default DateModal;
+
+const Container = styled.div`
+  width: 400px;
+  height: 500px;
+  background-color: black;
+`;
