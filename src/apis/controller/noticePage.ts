d3f361476ba8b637f4a7e29d3b93386b2e01c3d6
ppacman <smwfkim2@naver.com>
2023-12-18 18:06:08 +0900

    myPage 공지글 관련 api 작업(생성,삭제,수정,조회) #67

--- src/apis/controller/noticePage.ts
+++ src/apis/controller/noticePage.ts
@@ -1,10 +1,58 @@
 import sendApi from "../sendApi";
+import { PostNoticeParam } from "../../types/apiTypes";
 
-export const getNoticeList = async () => {
-  const res = await sendApi.get(`/notices/myStore`);
+export const getNoticeList = async (type: number, keyworld: String) => {
+  const res = await sendApi.get(
+    `/notices/myStore?type=${type}&keyword=${keyworld}`
+  );
   return res.data.content;
 };
 
-export const postNotice = async () => {
-  return await sendApi.post(`/notices/myStore`);
+export const deleteNotice = async (id: number) => {
+  try {
+    const response = await sendApi.delete(`/notices/${id}`);
+    if (response) {
+      alert("공지글 삭제가 완료되었습니다");
+    }
+  } catch (error) {
+    alert("잠시후에 다시 시도해주세요.");
+  }
+};
+
+export const modifyNotice = async (
+  { title, content, exposed, typeKey }: PostNoticeParam,
+  id: number
+) => {
+  try {
+    const response = await sendApi.put(`/notices/${id}`, {
+      title,
+      content,
+      exposed,
+      typeKey,
+    });
+  } catch (error) {
+    alert("잠시후에 다시 시도해주세요.");
+  }
+};
+
+export const postNoticeApi = async ({
+  title,
+  content,
+  exposed,
+  typeKey,
+}: PostNoticeParam) => {
+  try {
+    const response = await sendApi.post(`/notices/myStore`, {
+      title,
+      content,
+      exposed,
+      typeKey,
+    });
+
+    if (response) {
+      alert("공지글 작성이 완료되었습니다");
+    }
+  } catch (error) {
+    alert("잠시후에 다시 시도해주세요");
+  }
 };
