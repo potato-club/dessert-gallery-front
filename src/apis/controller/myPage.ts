451c66bb6cc4e86f3eca85fc05410763afe5fd69
donggyun <donggyunkim12@gmail.com>
2024-01-06 14:01:18 +0900

    feat(myPage.ts): 캘린더 이벤트등록, 메모등록 api 훅 구현 #89

--- src/apis/controller/myPage.ts
+++ src/apis/controller/myPage.ts
@@ -13,4 +13,21 @@ export const calendarPageApi = {
     );
     return res.data;
   },
+
+  async postCalendarSchedule(date: string, key: number) {
+    const res = await sendApi.post(`/stores/schedule`, {
+      date,
+      key,
+    });
+    return res;
+  },
+
+  async postCalendarMemo({ year, month }: DateInfo, content: string) {
+    const res = await sendApi.post(`/stores/memo`, {
+      content,
+      year,
+      month: month.toString().padStart(2, "0"),
+    });
+    return res;
+  },
 };
--- src/hooks/useSchedule.ts
+++ src/hooks/useSchedule.ts
@@ -1,8 +1,7 @@
 import { useState } from "react";
-import { useQuery } from "react-query";
+import { useMutation, useQuery, useQueryClient } from "react-query";
 import { calendarPageApi } from "../apis/controller/myPage";
 import { DateInfo } from "../container/myPage/calendarPage";
-
 export interface MyPageCalendarDataType {
   year: number;
   month: number;
@@ -13,6 +12,10 @@ interface ScheduleType {
   id: number;
   date: string;
   type: "HOLIDAY" | "EVENT" | "RESERVATION";
+}
+interface AddEventType {
+  date: string;
+  key: number;
 }
 
 export const useGetSchedule = ({ options }: any) => {
@@ -30,4 +33,50 @@ export const useGetSchedule = ({ options }: any) => {
   );
 
   return { dateInfo, setDateInfo, calendarData };
+};
+
+export const modifyCalendarPage = {
+  useAddSchedule(dateInfo: DateInfo) {
+    const queryClient = useQueryClient();
+    const { mutate: scheduleAddFn } = useMutation(
+      ["schedule", dateInfo.year, dateInfo.month],
+      ({ date, key }: AddEventType) =>
+        calendarPageApi.postCalendarSchedule(date, key),
+      {
+        onSuccess: () => {
+          queryClient.refetchQueries([
+            "schedule",
+            dateInfo.year,
+            dateInfo.month,
+          ]);
+        },
+      }
+    );
+    return { scheduleAddFn };
+  },
+
+  useDeleteSchedule() {},
+
+  useAddMemo(dateInfo: DateInfo) {
+    const queryClient = useQueryClient();
+    const { mutate: memoAddFn } = useMutation(
+      ["schedule", dateInfo.year, dateInfo.month],
+      (content: string) => calendarPageApi.postCalendarMemo(dateInfo, content),
+      {
+        onSuccess: () => {
+          queryClient.invalidateQueries([]);
+          queryClient.refetchQueries([
+            "schedule",
+            dateInfo.year,
+            dateInfo.month,
+          ]);
+        },
+      }
+    );
+    return { memoAddFn };
+  },
+
+  useCheckMemo() {},
+
+  useDeleteMemo() {},
 };
