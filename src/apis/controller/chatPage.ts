df2592bd52d8e8777e481435aec660b54ee56c19
Joonhyung Choi <joonhyungaaa@gmail.com>
2024-01-12 04:44:52 +0900

    feat: 채팅 페이지 api 컨트롤러 작업 #114
    feat: 채팅 페이지 api 컨트롤러 작업

---
+++ src/apis/controller/chatPage.ts
@@ -0,0 +1,20 @@
+import sendApi from "../sendApi";
+
+export const getStoreInfo = async () => {
+  const response = await sendApi.get(`/stores`);
+  return response;
+};
+
+export const postChatRoom = async (storeId: number) => {
+  const response = await sendApi.post(`/mypage/room`, {
+    data: {
+      storeId: storeId,
+    },
+  });
+  return response;
+};
+
+export const getChatRoom = async () => {
+  const response = await sendApi.get(`/mypage/room?page=${1}`);
+  return response;
+};
