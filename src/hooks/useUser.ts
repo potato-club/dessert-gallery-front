0787f18aeeff946e16f87c282d5597f21b983689
donggyun <donggyunkim12@gmail.com>
2024-02-28 17:29:34 +0900

    feat(BookmarkPage): 북마크페이지 퍼블리싱 및 api 연동 구현 #148

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 import { JWTStateAtom } from "../recoil/login/JWTStateAtom";
 import { useRecoilValue } from "recoil";
 import { getLoginUserInfo } from "../apis/controller/myPage";
-import { useQuery } from "react-query";
+import { useInfiniteQuery, useQuery } from "react-query";
+import { getBookmark } from "../apis/controller/detailStore";
 
 export const useUserState = () => {
   const [isGuest, setIsGuest] = useState<boolean>(true);
@@ -22,4 +23,26 @@ export const useLoginUserInfo = () => {
     refetchOnWindowFocus: false,
   });
   return { data };
+};
+
+export const useInfinityMyBookmark = () => {
+  const fetchMyBookmark = async ({ pageParam = 1 }) => {
+    const result = await getBookmark(pageParam);
+    return {
+      result: result.content,
+      nextPage: pageParam + 1,
+      isLast: result.last,
+    };
+  };
+
+  const { data, fetchNextPage, hasNextPage, isLoading, refetch } =
+    useInfiniteQuery(["bookmark"], fetchMyBookmark, {
+      getNextPageParam: (lastPage, pages) => {
+        if (!lastPage.isLast) return lastPage.nextPage;
+        return undefined;
+      },
+      refetchOnWindowFocus: false,
+      retry: 1,
+    });
+  return { data, fetchNextPage, hasNextPage, isLoading, refetch };
 };
