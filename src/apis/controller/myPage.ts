caf02d159c42dcd5c098f76b71f62d728ee5dd24
donggyun <donggyunkim12@gmail.com>
2024-03-03 23:15:56 +0900

    feat(mypage-follow): followPage 구현 및 팔로우한 가게 조회 api 연동 #151

--- src/apis/controller/myPage.ts
+++ src/apis/controller/myPage.ts
@@ -4,3 +4,8 @@ export const getLoginUserInfo = async () => {
   const res = await sendApi.get(`/users`);
   return res.data;
 };
+
+export const getFollow = async (page: number) => {
+  const res = await sendApi.get(`/mypage/follow?page=${page}`);
+  return res.data;
+};
--- src/hooks/useFollowAction.ts
+++ src/hooks/useFollowAction.ts
@@ -1,5 +1,6 @@
-import { useMutation, useQueryClient } from "react-query";
+import { useInfiniteQuery, useMutation, useQueryClient } from "react-query";
 import { boardApiList } from "../apis/controller/boardPage";
+import { getFollow } from "../apis/controller/myPage";
 
 // 팔로우 상태와 팔로우/취소 함수를 관리하는 Hook
 export const useFollowAction = (storeId: number) => {
@@ -10,8 +11,9 @@ export const useFollowAction = (storeId: number) => {
     () => boardApiList.putUnfollow(storeId.toString()),
     {
       onSuccess: () => {
-        queryClient.invalidateQueries(["detailBoard", storeId]);
         queryClient.refetchQueries(["detailBoard", storeId]);
+        queryClient.refetchQueries(["follow"]);
+        alert("언팔로우 되었습니다.");
       },
     }
   );
@@ -20,10 +22,34 @@ export const useFollowAction = (storeId: number) => {
     () => boardApiList.postFollow(storeId.toString()),
     {
       onSuccess: () => {
-        queryClient.invalidateQueries(["detailBoard", storeId]);
         queryClient.refetchQueries(["detailBoard", storeId]);
+        queryClient.refetchQueries(["follow"]);
+        alert("팔로우 되었습니다.");
       },
     }
   );
   return { putUnFollowMutate, postFollowMutate };
 };
+
+export function useInfinityGetFollow() {
+  const fetchFollower = async ({ pageParam = 1 }) => {
+    const result = await getFollow(pageParam);
+    return {
+      result: result.content,
+      nextPage: pageParam + 1,
+      isLast: result.last,
+    };
+  };
+
+  const { data, fetchNextPage, hasNextPage, isLoading, refetch } =
+    useInfiniteQuery(["follow"], fetchFollower, {
+      getNextPageParam: (lastPage, pages) => {
+        console.log(lastPage);
+        if (!lastPage.isLast) return lastPage.nextPage;
+        return undefined;
+      },
+      refetchOnWindowFocus: false,
+      retry: 1,
+    });
+  return { data, fetchNextPage, hasNextPage, isLoading, refetch };
+}
