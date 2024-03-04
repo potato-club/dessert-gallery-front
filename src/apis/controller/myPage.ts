8ae68dd8518744b7f611454cd3cbe385dd28eb29
donggyun <donggyunkim12@gmail.com>
2024-03-04 18:09:08 +0900

    feat(myPage-blocked): blocked 페이지 퍼블리싱 및 블랙리스트 api 연동 #153

--- src/apis/controller/myPage.ts
+++ src/apis/controller/myPage.ts
@@ -9,3 +9,22 @@ export const getFollow = async (page: number) => {
   const res = await sendApi.get(`/mypage/follow?page=${page}`);
   return res.data;
 };
+
+export const getBlockedList = async (page: number) => {
+  const res = await sendApi.get(`/mypage/blacklist?page=${page}`);
+  return res.data;
+};
+
+export const postBlocked = async (userName: string) => {
+  const res = await sendApi.post(`/mypage/blacklist`, {
+    userName,
+  });
+  return res.data;
+};
+
+export const putUnBlocked = async (userName: string) => {
+  const res = await sendApi.put(`/mypage/blacklist`, {
+    userName,
+  });
+  return res.data;
+};
