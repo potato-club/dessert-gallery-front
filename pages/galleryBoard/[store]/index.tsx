901aeb5cb686483483a4b0f11d02958d05bda52a
donggyun <donggyunkim12@gmail.com>
2024-04-11 17:34:58 +0900

    feat(storePage): invalid storeId값 들어올때 /galleryboard 로 리다이렉트 구현 #33 #54

--- pages/galleryBoard/[store]/index.tsx
+++ pages/galleryBoard/[store]/index.tsx
@@ -1,12 +1,6 @@
-import React from "react";
-import StorePage from "../../../src/container/storePage/StorePage";
-import { GetServerSideProps } from "next";
-import { dehydrate, QueryClient } from "react-query";
-import {
-  getStoreAnnounce,
-  getStoreInfo,
-} from "../../../src/apis/controller/detailStore";
-import { calendarPageApi } from "../../../src/apis/controller/calendarPage";
+import React from 'react';
+import StorePage from '../../../src/container/storePage/StorePage';
+import { GetServerSideProps } from 'next';
 
 export interface StoreProps {
   storeId: number;
@@ -23,36 +17,10 @@ const Store: React.FC<StoreProps> = (props) => {
 export default Store;
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const queryClient = new QueryClient();
   const storeId = Number(context.params?.store);
-  // storeId가 invalid 값일때 갤러리보드로 라우팅
-  try {
-    const today = new Date();
-    const year = today.getFullYear();
-    const month = today.getMonth() + 1;
-
-    await queryClient.prefetchQuery(["storeInfo", storeId], () =>
-      getStoreInfo(storeId)
-    );
-    await queryClient.prefetchQuery(["schedule", year, month], () =>
-      calendarPageApi.getStoreCalendar(storeId, { year, month })
-    );
-    await queryClient.prefetchQuery(["storeAnnounce", storeId], () =>
-      getStoreAnnounce(storeId)
-    );
-
-    return {
-      props: {
-        dehydratedState: dehydrate(queryClient), // 초기 데이터 캐싱
-        storeId,
-      },
-    };
-  } catch (error) {
-    return {
-      redirect: {
-        destination: "/galleryBoard",
-        permanent: false,
-      },
-    };
-  }
+  return {
+    props: {
+      storeId,
+    },
+  };
 };
