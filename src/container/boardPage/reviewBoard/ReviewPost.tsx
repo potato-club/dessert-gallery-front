6457b6e5c58198a265f813bb015c19e2f85d8ebd
JeongYunMi <tamd5971@gmail.com>
2024-08-05 21:34:48 +0900

    fix: 작성된 리뷰가 하나일 때 생기는 출력 에러 수정

--- src/container/boardPage/reviewBoard/ReviewPost.tsx
+++ src/container/boardPage/reviewBoard/ReviewPost.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ReviewPostWrap, TitleText, InformationWrap, Summary, ContentsText, BoxContentsStart, ReviewWrap, Text, TopTextWrap, TagWrap} from './ReviewPost.style'
+import { ReviewPostWrap, TitleText, InformationWrap, Summary, ContentsText, BoxContentsStart, ReviewWrap, Text, TopTextWrap, TagWrap, NoneReview} from './ReviewPost.style'
 import SlideImage from '../../../components/SlideImage/SlideImage'
 import Rating from '../../../components/Rating'
 import Tag from '../../../components/Tag'
@@ -9,11 +9,13 @@ import type { ReviewPostValue } from '../../../types/componentsProps'
 export default function ReviewPost({storeId, width,height,title, imgSrc,summary, reviewList}:ReviewPostValue) {
 
   /**차후 분리 */
+
   let fir = new Date(reviewList[0].createDate);
-  let sec = new Date(reviewList[1].createDate);
+  let sec = reviewList[1]? new Date(reviewList[1].createDate): null;
   
-  let dateToString = [`${fir.getFullYear()}-${(fir.getMonth()) + 1}-${fir.getDate()}`,
-                      `${sec.getFullYear()}-${(sec.getMonth()) + 1}-${sec.getDate()}`,]
+  let dateToString = sec !== null ?( [`${fir.getFullYear()}-${(fir.getMonth()) + 1}-${fir.getDate()}`,
+                                     `${sec.getFullYear()}-${(sec.getMonth()) + 1}-${sec.getDate()}`,])
+                                  :([`${fir.getFullYear()}-${(fir.getMonth()) + 1}-${fir.getDate()}`,])
 
   const onClickMoreButton = () => {
     window.location.href = `/galleryBoard/${storeId}`
@@ -30,23 +32,29 @@ export default function ReviewPost({storeId, width,height,title, imgSrc,summary,
           <Summary size='15px'>{summary}</Summary>
 
         </InformationWrap>
-        <ReviewWrap>
-          <TopTextWrap >
-            <Text size='13px' color='#FF8D00' bold={true}>{reviewList[0].nickname}</Text>
-            <Text size='13px' color='#828282' marginRight={true}>{dateToString[0]}</Text>
-            <Rating size='medium' ratingValue={String(reviewList[0].score)}/>
-          </TopTextWrap>
-          <ContentsText size='11px' color='#000000' bold={true}>{reviewList[0].content}</ContentsText>
-        </ReviewWrap>
-
-        <ReviewWrap>
-          <TopTextWrap >
-            <Text size='13px' color='#FF8D00' bold={true}>{reviewList[1].nickname}</Text>
-            <Text size='13px' color='#828282' marginRight={true}>{dateToString[1]}</Text>
-            <Rating size='medium' ratingValue={String(reviewList[1].score)}/>
-          </TopTextWrap>
-          <ContentsText size='11px' color='#000000' bold={true}>{reviewList[1].content}</ContentsText>
-        </ReviewWrap> 
+        {
+          reviewList[0]&&
+          <ReviewWrap>
+            <TopTextWrap >
+              <Text size='13px' color='#FF8D00' bold={true}>{reviewList[0].nickname}</Text>
+              <Text size='13px' color='#828282' marginRight={true}>{dateToString[0]}</Text>
+              <Rating size='medium' ratingValue={String(reviewList[0].score)}/>
+            </TopTextWrap>
+            <ContentsText size='11px' color='#000000' bold={true}>{reviewList[0].content}</ContentsText>
+          </ReviewWrap>
+        }
+        {
+          reviewList[1]?
+          (<ReviewWrap>
+            <TopTextWrap >
+              <Text size='13px' color='#FF8D00' bold={true}>{reviewList[1].nickname}</Text>
+              <Text size='13px' color='#828282' marginRight={true}>{dateToString[1]}</Text>
+              <Rating size='medium' ratingValue={String(reviewList[1].score)}/>
+            </TopTextWrap>
+            <ContentsText size='11px' color='#000000' bold={true}>{reviewList[1].content}</ContentsText>
+          </ReviewWrap>):
+          (<NoneReview/>) 
+        }
         <TagWrap>
           <Tag width='164px' height='32px' title='더보기' fontSize='12px' clickAble={true} hoverCss={true} onClickHandler={onClickMoreButton}/>
         </TagWrap>
